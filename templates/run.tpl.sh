9528019587133c971e42819cde1e0f330c20e266
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2019-04-27 18:36:47 -0300

    Update run.tpl.sh

--- templates/run.tpl.sh
+++ templates/run.tpl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cd "{{ main_repo }}"
+cd "{{ main_repo_dest }}"
 ansible-playbook "{{ repo.platform }}.yml" -i "{{ repo.dest }}/external/hosts" -e "env_file={{ repo.dest }}/env/{{ repo.env_file }}" 
