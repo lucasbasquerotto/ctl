6b4398665746db0c6e9007e22c9fa46e18b50d4f
Lucas Basquerotto <lucasbasquerotto@example.com>
2019-07-09 19:48:10 -0300

    tmp dir

--- templates/run.tpl.sh
+++ templates/run.tpl.sh
@@ -6,6 +6,7 @@ MAIN_REPO="{{ repo_main_repo_dest }}"
 HOSTS="{{ repo_dest }}/var/hosts"
 ENV_FILE="{{ repo_dest }}/env/{{ repo.env_file }}"
 ENV_FILE_TMP="{{ repo_dest }}/var/{{ repo.env_file }}"
+TMP_DIR="{{ repo_dest }}/tmp"
 VAULT="{{ repo.use_vault | ternary('--vault-id workspace@prompt', '') }}"
 PLAYBOOK="{{ repo.platform }}.yml"
 
@@ -13,7 +14,8 @@ cmp="$(cmp --quiet "$ENV_FILE" "$ENV_FILE_TMP" && echo 0 || echo 1)"
 
 if [ "$FORCE" = "true" ] || [ "$cmp" -eq 1 ]; then
   cd "$MAIN_REPO"
-  ansible-playbook $VAULT "$PLAYBOOK" -i "$HOSTS" -e "env_file=$ENV_FILE" "$@"    
+  ansible-playbook $VAULT "$PLAYBOOK" -i "$HOSTS" \
+    -e "env_file=$ENV_FILE" -e "env_tmp_dir=$TMP_DIR" "$@"    
   cp "$ENV_FILE" "$ENV_FILE_TMP"
 else
   echo 'Already up to date'
