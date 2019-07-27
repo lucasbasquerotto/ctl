1cb682de92be7206874aad05e9237e8c67891d60
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2019-07-27 13:16:54 -0300

    new structure

--- templates/run.tpl.sh
+++ templates/run.tpl.sh
@@ -4,21 +4,36 @@ set -euo pipefail
 force="{{ repo_run_force }}"
 cloud_repo="{{ repo_cloud_repo_dest }}"
 hosts="{{ repo_dest }}/var/hosts"
+env_local_repo="{{ repo.local_repo }}"
 env_dir="{{ repo_env_dir }}"
 env_file="{{ repo_env_dir }}/{{ repo.env_file }}"
 env_file_tmp="{{ repo_dest }}/var/{{ repo.env_file }}"
 tmp_dir="{{ repo_dest }}/tmp"
 vault="{{ repo_use_vault | ternary('--vault-id workspace@prompt', '') }}"
 playbook="{{ repo.type }}.yml"
-env_local_pod_dir="{{ repo.local_pod_dir | default('') | ternary('-e local_pod_dir=' + repo.local_pod_dir, '') }}"
+
+{% set env_local_pod_dir = 
+  (repo.local_pod_dir is defined) 
+  | ternary('-e local_pod_dir="' + repo_base_dir_pod + '/' 
+  + (repo.local_pod_dir| default('')) + '"', '""') 
+  | default('""') 
+-%}
+
+env_local_pod_dir='{{ env_local_pod_dir }}'
 
 {% set env_local_app_dir_list = '' -%}
 
+{%- if repo.local_app_dir_list is defined %}
 {%- for local_app_dir in repo.local_app_dir_list %}
-  {% set env_local_app_dir_list = env_local_app_dir_list + '-e {{ local_app_dir.name }}={{ local_app_dir.dir }}' -%}
+{% set env_local_app_dir_list = 
+  env_local_app_dir_list 
+  + '-e local_app_dir_' + local_app_dir.name 
+  + '="' + repo_base_dir_app + '/' + local_app_dir.dir + '"' 
+-%}
+env_local_app_dir_list='{{ env_local_app_dir_list }}'
 {% endfor %}
+{% endif %}
 
-env_local_app_dir_list="{{ env_local_app_dir_list }}"
 run=1
 
 if [ "$force" != "true" ]; then
@@ -28,8 +43,8 @@ fi
 if [ "$run" -eq 1 ]; then
   cd "$cloud_repo"
   ansible-playbook $vault "$playbook" -i "$hosts" \
-    -e "env_file=$env_file" -e "env_dir=$env_dir" -e "env_tmp_dir=$tmp_dir" 
-    $env_local_pod_dir $env_local_app_dir_list "$@"    
+    -e "env_file=$env_file" -e "env_dir=$env_dir" -e "env_tmp_dir=$tmp_dir" \
+    -e env_local_repo="$env_local_repo" $env_local_pod_dir $env_local_app_dir_list "$@"    
   cp "$env_file" "$env_file_tmp"
 else
   echo 'Already up to date'
