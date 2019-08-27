3061270e6265e24c38badf520eed9a44e6c07ec7
lucasbasquerotto <lucasbasquerotto@gmail.com>
2019-08-26 21:19:27 -0300

    scripts - setup

---
+++ setup/README.md
@@ -0,0 +1,9 @@
+# Preparing an environment
+
+Execute the following to download the preparation script and run it:
+
+```bash
+curl -O ~/setup https://github.com/lucasbasquerotto/ansible-manager/blob/master/setup/prepare.sh
+chmod +x ~/setup
+~/setup
+```
---
+++ setup/prepare.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -eou pipefail
+
+default_dev="n"
+default_dir_path="$HOME/dev"
+default_pod_dir_name="lrd-pod"
+default_git_repo_ctl="https://github.com/lucasbasquerotto/ansible-manager"
+
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+    
+start="$(date '+%F %X')"
+
+read -e -p "Is this a development environment? [Y/n] " yn
+dev=""
+
+if [[ $yn == "y" || $yn == "Y" ]]; then
+    dev=true
+    echo "development"
+else
+    echo "non-development"
+fi
+
+read -e -p "Enter the directory path: " -i "$default_dir_path" dir_path
+mkdir -p "$dir_path"
+
+cd "$dir_path/"
+
+read -e -p "Enter the pod directory name to run at the end of the setup: " \
+    -i "$default_pod_dir_name" pod_dir_name
+
+read -e -p "Enter the controller git repository: " \-i "$default_git_repo_ctl" git_repo_ctl
+git clone "$git_repo_ctl" ctl
+
+if [ "$dev" = true ]; then
+    echo -e "${CYAN}$(date '+%F %X') setup (dev) started${NC}"
+    ./ctl/run setup-dev
+    echo -e "${CYAN}$(date '+%F %X') setup (dev) ended${NC}"
+
+    echo -e "${CYAN}$(date '+%F %X') pod migration started${NC}"
+    ./pod/"$pod_dir_name"/run migrate
+    echo -e "${CYAN}$(date '+%F %X') pod migration ended${NC}"
+else
+    echo -e "${CYAN}$(date '+%F %X') setup started${NC}"
+    ./ctl/run setup
+    echo -e "${CYAN}$(date '+%F %X') setup ended${NC}"
+
+    echo -e "${CYAN}$(date '+%F %X') updating the environment repositories files${NC}"
+    ./ctl/run main-cmd /root/ctl/run run -e env_pod_name="$pod_dir_name"
+    echo -e "${CYAN}$(date '+%F %X') environment repositories files updated${NC}"
+fi
+
+end="$(date '+%F %X')"
+echo -e "${CYAN}ended - $start - $end${NC}"
