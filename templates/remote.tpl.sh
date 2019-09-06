cbda8db485ced2a7f9c0c673b51b29c076cbf3a0
Lucas Basquerotto <lucasbasquerotto@example.com>
2019-09-05 21:47:41 -0300

    deploy-pod command

--- templates/remote.tpl.sh
+++ templates/remote.tpl.sh
@@ -7,7 +7,4 @@ pod_local_name="$1"
 shift;
 
 cd "$main_dir"
-./run main-cmd ./run run -e env_pod="$pod_local_name"
-./run main-cmd /root/r/w/"$pod_local_name"/run "${@}"
-
-# ./run dev-cmd /root/r/w/main/run -e 'env_cmd="./remote.sh wordpress-blog-1"'
+./run deploy-pod "${@}"
