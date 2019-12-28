d05c7c83d596da4c96b6011dd9e5dc3781ac31bd
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2019-12-28 11:34:22 -0300

    run - local

--- templates/run.tpl.sh
+++ templates/run.tpl.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+#############################################################
+##################### Define Variables ######################
+#############################################################
+
 force="{{ repo_run_force }}"
 cloud_repo="{{ repo_cloud_repo_dest }}"
 repo="{{ repo_dest }}"
@@ -12,22 +16,38 @@ env_file_tmp="$repo/var/{{ repo.env_file }}"
 tmp_dir="$repo/tmp"
 vault="{{ repo_force_vault | ternary('--vault-id workspace@prompt', '') }}"
 vault_file="$repo/var/vault"
+env_local_data_dir_rel="../../data"
 playbook="{{ repo.type }}.yml"
 
 if [ -f "$vault_file" ]; then
   vault="--vault-id $vault_file"
 fi
 
-{% set env_local_pod_dir = 
-  (repo.local_pod_dir is defined) 
-  | ternary(
-  '-e local_pod_dir_rel="' + (repo.local_pod_dir | default('')) + '" ' +
-  '-e local_pod_dir="' + repo_base_dir_pod + '/' + (repo.local_pod_dir | default('')) + '"'
-  , '') 
-  | default('') 
--%}
+#############################################################
+######### Map Pod Directories for Local Development #########
+#############################################################
 
-env_local_pod_dir='{{ env_local_pod_dir }}'
+env_local_pod_dir_list=''
+
+{% set env_local_pod_dir_list = '' -%}
+
+{%- if repo.local_pod_dir_list is defined %}
+{%- for local_pod_dir in repo.local_pod_dir_list %}
+{% set env_local_pod_dir_list = 
+  env_local_pod_dir_list 
+  + '-e local_pod_dir_rel_' + local_pod_dir.name 
+  + '="' + local_pod_dir.dir + '" ' 
+  + '-e local_pod_dir_' + local_pod_dir.name 
+  + '="' + repo_base_dir_pod + '/' + local_pod_dir.dir + '" ' 
+-%}
+env_local_pod_dir_list='{{ env_local_pod_dir_list }}'
+{% endfor %}
+{% endif %}
+
+#############################################################
+######### Map App Directories for Local Development #########
+#############################################################
+
 env_local_app_dir_list=''
 
 {% set env_local_app_dir_list = '' -%}
@@ -39,11 +59,15 @@ env_local_app_dir_list=''
   + '-e local_app_dir_rel_' + local_app_dir.name 
   + '="' + local_app_dir.dir + '" ' 
   + '-e local_app_dir_' + local_app_dir.name 
-  + '="' + repo_base_dir_app + '/' + local_app_dir.dir + '"' 
+  + '="' + repo_base_dir_app + '/' + local_app_dir.dir + '" ' 
 -%}
 env_local_app_dir_list='{{ env_local_app_dir_list }}'
 {% endfor %}
 {% endif %}
+
+#############################################################
+################### Execute the Playbook ####################
+#############################################################
 
 run=1
 
@@ -55,8 +79,12 @@ if [ "$run" -eq 1 ]; then
   cd "$cloud_repo"
 
   ansible-playbook $vault "$playbook" -i "$hosts" \
-    -e "env_file=$env_file" -e "env_dir=$env_dir" -e "env_tmp_dir=$tmp_dir" \
-    -e env_local_repo="$env_local_repo" $env_local_pod_dir $env_local_app_dir_list "${@}"    
+    -e env_file="$env_file" \
+    -e env_dir="$env_dir" \
+    -e env_tmp_dir="$tmp_dir" \
+    -e env_local_repo="$env_local_repo" \
+    -e env_local_data_dir_rel="$env_local_data_dir_rel" \
+    $env_local_pod_dir_list $env_local_app_dir_list "${@}"    
   cp "$env_file" "$env_file_tmp"
 else
   echo 'Already up to date'
