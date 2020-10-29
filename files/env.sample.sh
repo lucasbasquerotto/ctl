48d92984f368a29084afd0ea4d5a50103f15ec36
lucasbasquerotto <lucasbasquerotto@gmail.com>
2020-10-29 16:05:24 +0000

    [dev] allow_container_type & shellcheck

---
+++ files/env.sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export container=''
+
+# optionals:
+
+# export container_type=''
+# export allow_container_type=''
+# export root=''
+# export subuser_group=''
+# export subuser_prefix=''
